572cfa66e237fb197fdace1071a8cff9d83d2119
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-01-28 20:06:11 +0600

    dynamic types of data in javascript

---
+++ javaScript/dynamicDataType.js
@@ -0,0 +1,14 @@
+let name = 'nayan';
+console.log(typeof(name));
+
+name = 23;
+console.log(typeof(name));
+
+name = 23.5;
+console.log(typeof(name));
+
+let number;
+console.log(typeof(number));
+
+let ami = null;
+console.log(typeof(ami));
