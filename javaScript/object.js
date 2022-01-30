1a4a2c84493ba3992245c5c5096475f0ac032252
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-01-30 12:14:59 +0600

    object in javascript

---
+++ javaScript/object.js
@@ -0,0 +1,16 @@
+let person= {
+    name: 'nayan',
+    age: 23,
+    nationality: 'Bangladeshi'
+};
+
+console.log(person);
+
+// Dot notation
+console.log(person.name);
+
+// Bracket notation
+console.log(person['nationality']);
+
+let boyos='age';
+console.log(person[boyos]);
