65c935e1f4ebdce1beeea2b337d295df83b01253
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-01-30 12:51:21 +0600

    function in javascripte

---
+++ javaScript/function.js
@@ -0,0 +1,12 @@
+
+function oddEven(number) {
+    if(number&1) {
+        console.log(number+' is a odd number')
+    }
+    else {
+        console.log(number+' is a even number')
+    }
+}
+
+oddEven(3);
+oddEven(1002);
