5e66b92376e792243bf53b6b3f1f7400cef5838a
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-06 23:29:37 +0600

    added props with key

--- src/input/Input.jsx
+++ src/input/Input.jsx
@@ -1,7 +1,11 @@
+function ListItem(props){
+    return <li>{props.value}</li>
+}
 function Input(props){
     const numbers = props.numbers;
     const listItems = numbers.map((number)=>
-        <li>{number}</li>
+        <ListItem key={number.toString()}
+            value={number} />
     );
     return(
         <ul>{listItems}</ul>
