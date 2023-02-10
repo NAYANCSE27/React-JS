a8f17b78a4c08d1723b1d2964236d248778f353d
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-10 15:30:42 +0600

    added counter app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import Input from './input/Input';
 import './App.css';
+import Counter from './counter/Counter';
 
 function App() {
-  const posts = [
-    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-  ];
-
   return (
     <div className="App">
-      <Input posts = {posts}/>
+      <Counter />
     </div>
   );
 }
